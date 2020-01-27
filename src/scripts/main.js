d393e75d54ab2c1dcde3bf81b9fda391133f9ad4
BryanNilsen <bryannilsen@gmail.com>
2020-01-27 09:31:29 -0600

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,15 @@ import { apiManager } from "./apiManager.js";
 import * as restaurantManager from "./restaurantManager.js";
 import * as artworkManager from "./artworkManager.js";
 import { getItineraryAndRenderToDOM } from "./itineraryManager.js"
+
+
 // apiManager.getMuseums();
 // apiManager.getParks();
 // apiManager.getTickets();
 // apiManager.getRestaurants("chicken");
 
 getItineraryAndRenderToDOM();
+
+// add current date to page
+const moment = moment().format("dddd, MMM Do, YYYY");
+document.getElementById("moment").append(moment)
